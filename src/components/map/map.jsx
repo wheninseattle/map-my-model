1941d8d3eab490176995235ba2a8ad7ed26c00c7
Weston Norwood <weston.norwood@gmail.com>
2023-03-20 23:37:46 -0700

    feat: Toggle map styles

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { useRef, useState, useEffect } from "react";
-import styles from "@/styles/Map.module.css";
-import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-mapboxgl.accessToken =
-  "pk.eyJ1Ijoid2hlbmluc2VhdHRsZSIsImEiOiJjbGZoaHlzMGwwNnBmM3hsamd1MWI0cHNtIn0.4-cC5OgYn3Sy93DB7F4Gpg";
-// mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN;
 
-const defaultMapCenter = [47.6165, -122.3548];
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+
+import styles from "@/styles/Map.module.css";
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+
+const defaultMapCenter = [47.6165, -122.3548]; // Olympic Sculpture Park, Seattle
 const [defaultLat, defaultLng] = defaultMapCenter;
 const defaultZoomLevel = 9;
+const defaultPitch = 60;
+
+const mapStyles = {
+  light: "mapbox://styles/mapbox/light-v11",
+  dark: "mapbox://styles/mapbox/dark-v11",
+  satellite: "mapbox://styles/mapbox/satellite-streets-v12"
+}
 
 export default function Map() {
   //Map default state
@@ -18,6 +26,13 @@ export default function Map() {
   const [lng, setLng] = useState(defaultLng);
   const [lat, setLat] = useState(defaultLat);
   const [zoom, setZoom] = useState(defaultZoomLevel);
+  const [mapStyle, setMapStyle] = useState(mapStyles.dark);
+
+  useEffect(() => {
+    if (map.current) {
+      map.current.setStyle(mapStyle);
+    }
+  }, [mapStyle]);
 
   useEffect(() => {
     if (map.current) {
@@ -27,18 +42,17 @@ export default function Map() {
         setZoom(map.current.getZoom().toFixed(2));
       });
     } else {
-
       map.current = new mapboxgl.Map({
         container: mapContainer.current,
-        style: "mapbox://styles/mapbox/light-v11",
-        // style: 'mapbox://styles/mapbox/satellite-streets-v12',
-        // style: "mapbox://styles/mapbox/dark-v11",
+        style: mapStyle,
         center: [lng, lat],
         zoom: zoom,
+        pitch: defaultPitch,
+        antialias: true,
       });
 
-      // Programmatically remove all map labels per instructions 
-      map.current.on("load", () => {
+      map.current.on("style.load", () => {
+        // Programmatically remove all map labels per instructions
         const mapLayers = map.current.style._layers;
         for (const layer in mapLayers) {
           if (mapLayers[layer].type === "symbol") {
@@ -46,9 +60,49 @@ export default function Map() {
             map.current.removeLayer(mapLayers[layer].id);
           }
         }
+        console.log(map.current)
+        // Load 3D buildings
+        map.current.addLayer({
+          id: "3d-buildings",
+          source: "composite",
+          "source-layer": "building",
+          filter: ["==", "extrude", "true"],
+          type: "fill-extrusion",
+          minzoom: 12,
+          paint: {
+            "fill-extrusion-color": "#ECF0F1",
+
+            // Use an 'interpolate' expression to
+            // add a smooth transition effect to
+            // the buildings as the user zooms in.
+            "fill-extrusion-height": [
+              "interpolate",
+              ["linear"],
+              ["zoom"],
+              15,
+              0,
+              15.05,
+              ["get", "height"],
+            ],
+            "fill-extrusion-base": [
+              "interpolate",
+              ["linear"],
+              ["zoom"],
+              15,
+              0,
+              15.05,
+              ["get", "min_height"],
+            ],
+            "fill-extrusion-opacity": 0.8,
+          },
+        });
       });
     }
   });
+
+  const onToggleMapMode = () => {
+    setMapStyle(mapStyle===mapStyles.dark ? mapStyles.satellite : mapStyles.dark);
+  };
 
   return (
     <div>
@@ -56,6 +110,9 @@ export default function Map() {
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
       <div ref={mapContainer} className={styles.mapContainer}></div>
+      <button onClick={onToggleMapMode} className={styles.dataPanel}>
+        Toggle
+      </button>
     </div>
   );
 }
